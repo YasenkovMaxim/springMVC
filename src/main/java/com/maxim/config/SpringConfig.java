4c72198f264e1848efb0517859de915634c88e30
jasen <jasenkoff.mack@yandex.ru>
2026-02-09 19:01:01 +0300

    Initial commit

---
+++ src/main/java/com/maxim/config/SpringConfig.java
@@ -0,0 +1,20 @@
+package com.maxim.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.maxim")
+public class SpringConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}

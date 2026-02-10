73d2fe042cbb53d4942254ec4c1bd054ef5ac186
jasen <jasenkoff.mack@yandex.ru>
2026-02-10 20:07:48 +0300

    Spring MVC 4

---
+++ src/main/java/com/maxim/interceptor/SecurityInterceptor.java
@@ -0,0 +1,25 @@
+package com.maxim.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SecurityInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("PreHandle: " + request.getRequestURL());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("PostHandle: " + request.getRequestURL());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("AfterHandle: " + request.getRequestURL());
+    }
+}
